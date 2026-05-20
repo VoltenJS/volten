cb16d7d2804591232a3d272704c5e408985f9888
Insanerx <290829829+insanerx@users.noreply.github.com>
2026-05-20 13:43:34 +0300

    Added app.ts

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { App } from "./core/server.ts";
+const volten = new App();
+const app = volten.host("**");
+
+app.get("/", (ctx) => {
+  ctx.send("Hello World!");
+});
+
+app.get("/user/:id", (ctx) => {
+  const userId = ctx.params.id;
+  ctx.json({ userId });
+});
+
+app.post("/data", async (ctx) => {
+  const data = await ctx.body();
+  ctx.json({ received: data });
+});
+
+volten.listen(3000, () => {
+  console.log("Server is running on port 3000 at: http://localhost:3000");
+});
