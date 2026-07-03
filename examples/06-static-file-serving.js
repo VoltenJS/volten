7e121e85bf97eb1619fa272ced13920d000d40ee
insanerx <290829829+insanerx@users.noreply.github.com>
2026-07-03 22:31:04 +0300

    Removed multi-tenancy from examples

--- examples/06-static-file-serving.js
+++ examples/06-static-file-serving.js
@@ -2,8 +2,7 @@ import { App } from "../dist/core/server.js";
 import path from "path";
 import { fileURLToPath } from "node:url";
 
-const volten = new App();
-const app = volten.host("**");
+const app = new App();
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,6 +14,6 @@ app.get("/", (ctx) => {
   ctx.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-volten.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Static File Serving demo running on http://localhost:${PORT}`);
 });
